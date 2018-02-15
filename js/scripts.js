da3f9caa64031120d6455c4abaf6583dc27bf450
pawelz <t03c10@gmail.com>
2018-02-15 21:27:35 +0100

    task corrected' '

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,10 @@
 var list = document.getElementById('list');
 var addBtn = document.getElementById('addElem');
 
-addBtn.addEventListener('click', function() {
-var element = document.createElement('li');
-element.innerHTML = 'item ' + (document.getElementsByTagName('li').length);
-list.appendChild(element)});
+addBtn.addEventListener('click', 
+    function() {
+    var element = document.createElement('li');
+    element.innerHTML = 'item ' + (document.getElementsByTagName('li').length);
+    list.appendChild(element);
+    }
+)
